d7c987385449a36192a5f5a3953b66356a9618e4
Gabriel dos Santos <109832618+gabrielopesdev@users.noreply.github.com>
2023-11-09 00:23:54 +0000

    clear list e onValue

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js"
 
-import {getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/10.5.2/firebase-database.js"
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://shopping-app-ab654-default-rtdb.firebaseio.com/"
@@ -9,14 +8,40 @@ const appSettings = {
 
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
-const itensInDB = ref(database, "list")
+const itensInDB = ref(database, "listShop")
 
 const inputTextEl = document.querySelector("#add-item")
 const buttonAddEl = document.querySelector("#add-button")
+const shoppingListEl = document.querySelector("#shopping-list") 
 
 buttonAddEl.addEventListener("click", ()=>{
 
-    let inputTextValue = inputTextEl.value
-    push(itensInDB, inputTextValue)
+    let inputValue = inputTextEl.value
+    push(itensInDB, inputValue)
 
+    clearInputFieldEl(inputTextEl)
+    clearShoppingListEl()
+    
+    onValue(itensInDB, (list)=>{
+
+        var listArray = Object.values(list.val())
+            
+        for(let i=0; i < listArray.length; i++) {
+            addItemToList(listArray[i])
+        }   
+    })
 })
+
+function clearInputFieldEl(input) {
+    input.value = ""
+}
+
+function addItemToList(value) {
+    shoppingListEl.innerHTML += `<span>${value}</span>`
+}
+
+function clearShoppingListEl() {
+    shoppingListEl.innerHTML = ""
+}
+
+
